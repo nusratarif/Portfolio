85376083bbcfb2214a270202bf61f25260aa2072
nusratarif <nusratarif02@gmail.com>
2021-09-04 22:56:23 +0500

    updated src/Components/Project.js

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import img1 from './images/D1.png';
-import img2 from './images/D2.png';
-import img3 from './images/D3.png';
+import img2 from './images/D3.png';
+import img3 from './images/D2.png';
 import img4 from './images/D4.jpeg';
 import img5 from './images/D5.jpeg';
 import img6 from './images/D6.jpeg';
@@ -29,38 +29,38 @@ const Project = () => {
                         <div className="project-img-box">
                             <img className="img1" src={img1} alt="P2" height="160" width="160" />
                             <h4>HTML CSS</h4>
-                            <h5><a href="https://responsive-html-website-e1e457.netlify.app">Live Preview</a></h5>
+                            <h5><a target="_blank" href="https://responsive-html-website-e1e457.netlify.app">Live Preview</a></h5>
                         </div>
 
                         <div className="project-img-box">
                             <img className="img2" src={img2} alt="P2" height="160" width="160" />
                             <h4>HTML CSS</h4>
-                            <h5><a href="">Live Preview</a></h5>
+                            <h5><a target="_blank" href="https://my-html-demo-app-3f019c.netlify.app">Live Preview</a></h5>
                         </div>
 
                         <div className="project-img-box">
                             <img className="img3" src={img3} alt="P2" height="160" width="160" />
                             <h4>HTML CSS</h4>
-                            <h5><a href="https://demo-html-app-74bede.netlify.app/">Live Preview</a></h5>
+                            <h5><a target="_blank" href="https://demo-html-app-74bede.netlify.app/">Live Preview</a></h5>
                         </div>
 
                         <div className="project-img-box">
                             <img className="img4" src={img4} alt="P1" height="160" width="160" />
 
                             <h4>Expense Tracker App</h4>
-                            <h5><a href="https://expanse-tracker-661963.netlify.app/">Live Preview</a></h5>
+                            <h5><a target="_blank" href="https://expanse-tracker-661963.netlify.app/">Live Preview</a></h5>
                         </div>
 
                         <div className="project-img-box">
                             <img className="img6" src={img6} alt="P2" height="160" width="160" />
                             <h4>Shoe Store</h4>
-                            <h5><a href="https://online-shoestore-2064ee.netlify.app">Live Preview</a></h5>
+                            <h5><a target="_blank" href="https://online-shoestore-2064ee.netlify.app">Live Preview</a></h5>
                         </div>
 
                         <div className="project-img-box">
                             <img className="img5" src={img5} alt="P4" height="160" width="160" />
                             <h4>React Quize</h4>
-                            <h5><a href="https://project-quizapp-627188.netlify.app/">Live Preview</a></h5>
+                            <h5><a target="_blank" href="https://project-quizapp-627188.netlify.app/">Live Preview</a></h5>
                         </div>
                     </div>
                 </div>
